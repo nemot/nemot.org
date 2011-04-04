296aae811bdbdf917574efa887ebf90caa597b0a
nemot <nemot@greenline-trans.co.uk>
2011-04-04 09:42:33 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NemotOrg::Application.config.secret_token = '64e649f6b4ed0740327ad797c5743752221d06c40698584965458b05012b714c3f36b11664c27e14948f7143dc98b9bdac6579790588a43ead92448cf154b835'
